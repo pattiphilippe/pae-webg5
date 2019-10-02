0bae570fb27be12c2661f175d3112c4fab33f71d
Philippe Patti <43197@etu.he2b.be>
2019-10-02 15:50:33 +0200

    Page web avec titre, css bidon qui fonctionne pas

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController{
+
+    @GetMapping("/")
+    public String home(Model model){
+        model.addAttribute("username", "Philippe");
+        return "home";
+    }
+
+}
