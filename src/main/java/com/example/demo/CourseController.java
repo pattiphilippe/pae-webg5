617c8b7f96032c5d83fa63a8f5393f17b08c4d58
Philippe Patti <43197@etu.he2b.be>
2019-11-07 12:21:23 +0100

    Ajout cours possible dans db grace au formulaire

--- src/main/java/com/example/demo/CourseController.java
+++ src/main/java/com/example/demo/CourseController.java
@@ -4,16 +4,16 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import com.example.demo.business.CourseDB;
+import com.example.demo.business.EtudiantDB;
 import com.example.demo.business.PAEService;
 import com.example.demo.model.Course;
-import com.example.demo.model.Etudiant;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.client.RestTemplate;
@@ -22,41 +22,34 @@ import org.springframework.web.client.RestTemplate;
 @SessionAttributes("courses")
 public class CourseController{
 
-    @Autowired 
-    private PAEService pae;
-
-    @ModelAttribute(name = "courses")
-    public List<Course> coursesAttr(Model model){
-        return pae.getCourses();
-    }
-
-    @ModelAttribute(name = "etudiants")
-    public List<Etudiant> etudiantsAttr(Model model){
-        return pae.getEtudiants();
-    }
+    @Autowired
+    private CourseDB courseDB;
+    
+    @Autowired
+    private EtudiantDB etudiantDB;
 
     @GetMapping("/courses")
     public String courses(Model model){
         model.addAttribute("newCourse", new Course());
-        //commentaire
+        model.addAttribute("courses", courseDB.findAll());
+        model.addAttribute("etudiants", etudiantDB.findAll());
         return "courses";
     }
 
-    @PostMapping("/urlCtrl")
-    public String doSomething( @Valid Course newCourse, Errors errors){
+    @PostMapping("/addNewCourse")
+    public String doSomething(Model model, @Valid Course newCourse, Errors errors){
         if(errors.hasErrors()){
             return "redirect:/courses";
         }
-        pae.addCourse(newCourse);
+        courseDB.save(newCourse);
+        model.addAttribute("courses", courseDB.findAll());
         return "redirect:/courses";
     }
 
     @GetMapping("/courses/json")
     public List<Course> getCoursesJson(){
-        System.out.println("JLC ------ DEBUT APPEL SERVICE");
         RestTemplate restTemplate = new RestTemplate();
         PAEService pae = restTemplate.getForObject("http://localhost:8080/api/getPae", PAEService.class);
-        System.out.println("JLC ------ FIN APPEL SERVICE");
         return pae.getCourses();
     }
 }
