b214f7dca00e137a617b0b76a8bac6dd6d1c7fbe
Philippe Patti <43197@etu.he2b.be>
2020-01-06 09:42:23 +0100

    Recap project added

---
+++ recap/src/test/java/patti/philippe/recap/RecapApplicationTests.java
@@ -0,0 +1,13 @@
+package patti.philippe.recap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
