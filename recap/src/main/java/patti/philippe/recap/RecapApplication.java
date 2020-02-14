cac2af3e3ebe76d98efa5e2e68f879b47cf73f4e
Philippe Patti <43197@etu.he2b.be>
2020-02-14 15:54:08 +0100

    Menu secured - start of error pages

--- recap/src/main/java/patti/philippe/recap/RecapApplication.java
+++ recap/src/main/java/patti/philippe/recap/RecapApplication.java
@@ -1,12 +1,9 @@
 package patti.philippe.recap;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import lombok.extern.slf4j.Slf4j;
-import patti.philippe.recap.model.AnnualProgram;
-import patti.philippe.recap.repository.StudentRepository;
 
 @Slf4j
 @SpringBootApplication
